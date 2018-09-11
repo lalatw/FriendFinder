29a82d8c774fb59ed1d1fda0e55a6eced9b7438a
L Lee <lauralee.sh@gmail.com>
2018-09-10 18:55:37 -0700

    updated home.html, htmlRoutes, apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,11 +5,11 @@ module.exports=function(app) {
     //GET route with the url /api/friends that will display JSON of all possible friends.
     app.get("/api/friends", function(req, res) {
         res.json(friendData);
-    })
+    });
 
     //POST routes /api/friends that will handle incoming survey results and do the the compatibility. 
-    app.post("/api/friends", function(req, res) {
-        if (friendData.)
+    // app.post("/api/friends", function(req, res) {
+    //     if (friendData.)
 
 
 
@@ -17,7 +17,7 @@ module.exports=function(app) {
 
 
 
-    })
+    // })
 
 
 };
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
   });
