f87de54ab3a570b46766aa58143e78f1b12ec385
L Lee <lauralee.sh@gmail.com>
2018-09-10 23:02:48 -0700

    updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,17 +7,51 @@ module.exports=function(app) {
         res.json(friendData);
     });
 
+
     //POST routes /api/friends that will handle incoming survey results and do the the compatibility. 
-    // app.post("/api/friends", function(req, res) {
-    //     if (friendData.)
+    app.post("/api/friends", function(req, res) {
+
+        console.log(req.body);
+        friendData.push(req.body);
+        
+        var currentPerson = friendData[friendData.length-1];
+        //console.log(friendData.length);
+        //console.log("current person:"+currentPerson);
+
+        //the temp closest match person's index
+        var closestMatchPerson = 0;
+
+        //the temp closest match person's score
+        var closestScore = 0;
+
+        for (var i=0; i<friendData.length-1; i++) {
+ 
+            var scoreDiff = compare(currentPerson, friendData[i]);
+
+            if (scoreDiff<closestScore) {
+                closestScore = scoreDiff;
+                closestMatchPerson = i;
+            }
+        }
+
+        console.log(friendData[closestMatchPerson]);
+        res.json(friendData[closestMatchPerson]);
 
 
 
+    });
 
 
-
-
-    // })
+    //the function to compare the score of current person with others' score
+    function compare (currentPerson, otherObj) {
+        var scoreDifference = 0;
+        for (var i=0; i<friendData.length; i++) {
+            scoreDifference+=Math.abs(parseInt(currentPerson.scores[i])-parseInt(otherObj.scores[i]));
+        }
+        return scoreDifference;
+    }
 
 
 };
+
+
